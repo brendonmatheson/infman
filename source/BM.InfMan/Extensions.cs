e615938a290e8954358476af0c7fde9f19cb2dc9
Brendon Matheson <b@brendonmatheson.com>
2016-06-23 19:35:34 +0700

    Changed license terms from default proprietary to GNU GPLv2.

--- source/BM.InfMan/Extensions.cs
+++ source/BM.InfMan/Extensions.cs
@@ -1,14 +1,19 @@
 ﻿/*
     Infrastructure Manager
+	http://bren.cc/infman
 
 	Copyright (c) 2016, Brendon Matheson
 
-	http://bren.cc/infman
+    This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any
+    later version.
 
-	This work is copyright.  You may not reproduce or transmit it any any
-	form or by any means without permission in writing from the owner of this
-	work, Brendon Matheson.  If you infringe our copyright, you render yourself
-    liable for prosecution.
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+    details.
+
+    You should have received a copy of the GNU General Public License along with this program; if not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 namespace cc.bren.infman
